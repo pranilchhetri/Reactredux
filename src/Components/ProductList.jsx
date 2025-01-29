b0af2a17fbdab62075dc09189c45b82d7c52d41d
pranilchhetri <pranilchhetri2014@gmail.com>
2025-01-29 12:02:22 -0500

    implemented ui and reducer functions

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import './ProductList.css'; 
+import { useDispatch } from 'react-redux';
+import {useState} from 'react';
+import { addItemToCart } from './CartSlice';    
+
 
 const ProductList = () => {
+
+ const dispatch = useDispatch();
+  const [disabledProducts, setDisabledProducts] = useState([]);
 
   const products = [
     { id: 1, name: 'Product A', price: 60 },
     { id: 2, name: 'Product B', price: 75 },
     { id: 3, name: 'Product C', price: 30 },
   ];
+
+  const handleAddToCart = product => {
+    dispatch(addItemToCart(product));
+    setDisabledProducts([...disabledProducts,product.id]);
+  }
+
+ 
 
   return (
     <div className="product-list">
@@ -16,7 +30,11 @@ const ProductList = () => {
         {products.map((product)=>(
             <li key={product.key} className='product-list-item'>
                 <span>{product.name} - {product.price}</span>
-                <button>Add to Cart</button>
+                <button
+                    className={`add-to-cart-btn ${disabledProducts.includes(product.id)? 'disabled' : ''}`}
+                    onClick={()=> handleAddToCart(product)}
+                    disabled={disabledProducts.includes(product.id)}
+                    >Add to Cart</button>
             </li>
         ))}
       </ul>
